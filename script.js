25880ba3978f36d89fbee339f206d25bbd2564ff
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-18 16:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const inputText = document.getElementById('fname');
+
+inputText.addEventListener('blur', function() {
+  this.value = this.value.toUpperCase();
+});
+
